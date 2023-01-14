9aa197493b0157ed479b8b354c0bb7f2dd19b460
Akinfemiwa <possibleolayode5344@gmail.com>
2023-01-14 17:55:35 +0000

    create validation, area, and display functions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-
 from models.base import Base
-
 
 class Rectangle(Base):
     """
@@ -49,6 +47,8 @@ class Rectangle(Base):
     def y(self):
         """get y"""
         return self.__y
+
+		"""create setters with validation """
 
     @width.setter
     def width(self, value):
